728d97d5f03741aa77b006b416f15eb2b1a30d9c
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-08-03 16:50:46 +0200

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import Img from '../assets/logo.jpg';
+import { Link } from 'react-router-dom';
+
+const NavbarContainer = styled.div`
+    width: 100%;
+    height: 80px;
+    z-index: 20;
+    background: #fff;
+`;
+
+const NavbarWrap = styled.div`
+    width: 1200px;
+    height: 100%;
+    margin: 0 auto;
+    z-index: 20;
+    display: flex;
+    position: relative;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Logo = styled.div`
+    width: 128px;
+    height: 22px;
+    background: url(${Img}) 0 45% no-repeat;
+    background-color: 100% 100%;
+    cursor: pointer;
+`;
+
+const Nav = styled.nav`
+
+`;
+const NavLink = styled(Link);
+const Search = styled.div``;
+const SearchWrap = styled.div``;
+const Input = styled.input``;
+const ButtonContainer = styled.div``;
+const Button = styled.div``;
 
 const Navbar = () => {
     return (
@@ -19,7 +57,8 @@ const Navbar = () => {
                     </SearchWrap>
                 </Search>
                 <ButtonContainer>
-                    <Button>S</Button>
+                    <Button>SIGN UP</Button>
+                    <Button>LOGIN</Button>
                 </ButtonContainer>
             </NavbarWrap>
         </NavbarContainer>
